b011a3898e91e76b9c5858c41962201c692aa4ad
Vova <darkcosmo543@gmail.com>
2026-04-23 01:08:07 +0300

    feat: add StartSession logic and question shuffling to TestService

--- TestSimulator/TestSimulator.BLL/Services/TestService.cs
+++ TestSimulator/TestSimulator.BLL/Services/TestService.cs
@@ -28,4 +28,42 @@ public class TestService
     {
         _repository.DeleteTopic(topicId);
     }
+
+    public TestSession? StartSession(Guid testId)
+    {
+        var allTopics = _repository.GetAllTopics();
+        var test = allTopics.SelectMany(t => t.Tests).FirstOrDefault(t => t.Id == testId);
+
+        if (test == null)
+        {
+            return null;
+        }
+
+        var questionsForSession = test.Questions.ToList();
+
+        if (_config.ShuffleQuestions)
+        {
+            ShuffleList(questionsForSession);
+        }
+
+        return new TestSession
+        {
+            TestId = test.Id,
+            SessionQuestions = questionsForSession
+        };
+    }
+
+    private void ShuffleList<T>(List<T> list)
+    {
+        var random = new Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = random.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 }
